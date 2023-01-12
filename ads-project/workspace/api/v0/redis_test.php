6231a452a296da8228fd24fbc4aa0aa2e3087f60
DongWon Sehr <dongwon@its-newid.com>
2023-01-13 01:47:05 +0900

    + redis test

---
+++ ads-project/workspace/api/v0/redis_test.php
@@ -0,0 +1,32 @@
+<?php
+require_once("library/redis.php");
+require_once("library/mysql.php");
+
+extract($_GET);
+
+$m_redis = new dw_redis();
+
+if ($m_redis->connect() == true) {
+    $redis_version = $m_redis->redis->info()["redis_version"];
+
+    $m_mysql = new dw_mysql();
+    $r = $m_mysql->query("SELECT COUNT(*) FROM ad_campaigns;");
+    
+    $cache_key = "redis_test";
+    $m_redis->set($cache_key, $r);
+    
+    $cache = $m_redis->get($cache_key);
+    $response = [
+        "redis_version" => $redis_version,
+        "ad_campaigns_records_count" => $cache[0][0],
+    ];
+} else {
+    $response = [
+        "errorCode" => 404,
+        "message" => "Redis Failure!",
+    ];
+}
+
+header("Content-Type: application/json");
+echo json_encode($response);
+?>
---
+++ ads-project/workspace/library/redis.php
@@ -0,0 +1,139 @@
+<?php
+class dw_redis
+{
+    public $redis = NULL;
+    public $host = 'hostname-redis';
+    public $port = 6379;
+    public $key_prefix = "DEV_";
+    public $_stack = array();
+    public $handle = false;
+
+    function __construct()
+    {
+    }
+
+    function connect()
+    {
+        if ($this->handle == true) {
+            return true;
+        }
+
+        $this->redis = new Redis();
+
+        for ($i = 0; $i < 5; $i++) {
+            if ($i) {
+                sleep($i);
+            }
+
+            if ($this->handle == false) try {
+                $this->redis->connect($this->host, $this->port);
+                $this->handle = true;
+            } catch (Exception $e) {
+                $this->handle = false;
+            }
+
+            if ($this->handle == true) {
+                break;
+            }
+        }
+
+        if ($this->handle == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function get($key)
+    {
+        if ($this->connect() == true) {
+            // return NULL if key is empty
+            if ( ! strlen($key) ) {
+                return NULL;
+            }
+
+            $resp = $this->redis->get($this->key_prefix . $key);
+
+            // return NULL if response is empty
+            if ( ! strlen($resp) ) {
+                return NULL;
+            } else {
+                return json_decode($resp, true, JSON_UNESCAPED_UNICODE | JSON_THROW_ON_ERROR);
+            }
+        } else {
+            return NULL;
+        }
+    }
+
+    function set($key, $val, $timeout = 259200) // 86400 * 3 days
+    {
+        if ($this->connect() == true) {
+            // return NULL if key is empty
+            if ( ! strlen($key) ) {
+                return NULL;
+            }
+
+            $json_encoded_val = json_encode($val, JSON_UNESCAPED_UNICODE);
+
+            $result = $this->redis->set($this->key_prefix . $key, $json_encoded_val);
+
+            $this->expire($key, $timeout);
+
+            return $result;
+        } else {
+            return NULL;
+        }
+    }
+
+    function expire($key, $timeout = 259200) // 86400 * 3 days
+    {
+        if ($this->connect() == true) {
+            // return NULL if key is empty
+            if ( ! strlen($key) ) {
+                return NULL;
+            }
+
+            return $this->redis->expire($this->key_prefix . $key, $timeout);
+
+        } else {
+            return NULL;
+        }
+    }
+
+    function delete($key)
+    {
+        if ($this->connect() == true) {
+            
+            // return NULL if key is empty
+            if ( ! strlen($key) ) {
+                return;
+            }
+
+            /*
+                Usage Example:
+                    $m_redis->delete('hello'); // delete hello
+                    $m_redis->delete('*'); // delete all keys
+                    $m_redis->delete('hell*'); // delete hell*
+            */
+            if ((strpos($key, "*") == true) || ($key == "*")) {
+                $key_list = $this->redis->keys($this->key_prefix . $key);
+                if (@count($key_list)) {
+
+                    // delete key by list
+                    $this->redis->delete($key_list);
+
+                    /*
+                        below is slower than above
+                        --------------------------
+                        foreach($key_list as $val)
+                        {
+                            $this->redis->delete( $val);
+                        }
+                    */
+                }
+            } else {
+                $this->redis->delete($this->key_prefix . $key);
+            }
+        }
+    }
+}
