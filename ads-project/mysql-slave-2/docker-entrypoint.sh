75b931ebdf59e7b08736b9bd4dadcd9bb088d2cd
DongWon Sehr <dongwon@its-newid.com>
2023-01-15 05:01:45 +0900

    + slave-2 replication

--- ads-project/mysql-slave-2/docker-entrypoint.sh
+++ ads-project/mysql-slave-2/docker-entrypoint.sh
@@ -21,6 +21,7 @@ echo "start mysqld"
 systemctl start mysqld
 systemctl status mysqld
 
+
 echo ""
 echo "Running a preset option"
 echo "Create root user"
@@ -35,14 +36,31 @@ mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "create user '$MYSQL_USER'@'%' identified
 mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "grant all privileges on $MYSQL_DATABASE.* to '$MYSQL_USER'@'%'; FLUSH PRIVILEGES;"
 mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "select user, host FROM mysql.user;"
 
-echo "Create slave-1 user"
-mysql -uroot -p$MYSQL_ROOT_PASSWORD -S /var/lib/mysql/mysql.sock -e "create user 'repl-1'@'172.16.0.%' identified with mysql_native_password"
-mysql -uroot -p$MYSQL_ROOT_PASSWORD -S /var/lib/mysql/mysql.sock -e "alter user 'repl-1'@'172.16.0.%' identified by 'repl-1'"
-mysql -uroot -p$MYSQL_ROOT_PASSWORD -S /var/lib/mysql/mysql.sock -e "grant replication slave on *.* to 'repl-1'@'172.16.0.%'; FLUSH PRIVILEGES;"
-mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "select user, host FROM mysql.user;"
-
 echo "Create slave-2 user"
 mysql -uroot -p$MYSQL_ROOT_PASSWORD -S /var/lib/mysql/mysql.sock -e "create user 'repl-2'@'172.16.0.%' identified with mysql_native_password"
 mysql -uroot -p$MYSQL_ROOT_PASSWORD -S /var/lib/mysql/mysql.sock -e "alter user 'repl-2'@'172.16.0.%' identified by 'repl-2'"
 mysql -uroot -p$MYSQL_ROOT_PASSWORD -S /var/lib/mysql/mysql.sock -e "grant replication slave on *.* to 'repl-2'@'172.16.0.%'; FLUSH PRIVILEGES;"
 mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "select user, host FROM mysql.user;"
+
+## get  status
+master_log_file=`mysql -uroot -proot -h 172.16.0.12 -S /var/lib/mysql/mysql.sock -e "show master status\G" | grep mysql-bin`
+re="[a-z]*-bin.[0-9]*"
+
+if [[ ${master_log_file} =~ $re ]];then
+    master_log_file=${BASH_REMATCH[0]}
+fi
+echo "master_log_file: $master_log_file"
+
+master_log_pos=`mysql -uroot -proot -h 172.16.0.12 -S /var/lib/mysql/mysql.sock -e "show master status\G" | grep Position`
+
+re="[0-9]+"
+
+if [[ ${master_log_pos} =~ $re ]];then
+    master_log_pos=${BASH_REMATCH[0]}
+fi
+echo "master_log_pos: $master_log_pos"
+
+query="change master to master_host='172.16.0.12', master_user='repl-2', master_password='repl-2', master_log_file='${master_log_file}', master_log_pos=${master_log_pos}, master_port=3306"
+
+mysql -uroot -proot -S /var/lib/mysql/mysql.sock -e "${query}"
+mysql -uroot -proot -S /var/lib/mysql/mysql.sock -e "start slave"
